ec2d516fafb243ae9084ee310646146fa7119881
Narknon <narknon0@gmail.com>
2023-02-03 09:46:16 -0500

    update linetrace to use helpers; add gwt and TWD signature overrides

---
+++ CustomGameConfigs/Ghost Wire Tokyo/FName_Constructor.lua
@@ -0,0 +1,7 @@
+function Register()
+    return "48 89 5C 24 10 48 89 74 24 18 57 48 81 EC 40 04 00 00"
+end
+
+function OnMatchFound(MatchAddress)
+    return MatchAddress
+end
---
+++ CustomGameConfigs/Ghost Wire Tokyo/FName_ToString.lua
@@ -0,0 +1,7 @@
+function Register()
+    return "48 89 5C 24 08 48 89 6C 24 18 48 89 74 24 20 57 41 56 41 57 48 81 EC 50 08 00 00"
+end
+
+function OnMatchFound(MatchAddress)
+    return MatchAddress
+end
