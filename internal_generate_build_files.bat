770f9ce5955c2faf79400eeb658eada17ffa6e10
narknon <73571427+narknon@users.noreply.github.com>
2023-05-17 12:23:31 -0400

    Add build files for Clang + Ninja

--- internal_generate_build_files.bat
+++ internal_generate_build_files.bat
@@ -1,13 +1,30 @@
 set BuildMode=%1
+set Compiler=%2
 
 IF NOT DEFINED %BuildMode (
 set BuildMode=Release
 )
 
-if not exist "VS_Solution\" (
-	mkdir "VS_Solution"
+IF NOT DEFINED %Compiler (
+set Compiler=MSVC
 )
 
-cd internal_build_tools
-call generate_vs_solution_internal.bat %BuildMode%
+if %Compiler% == MSVC (
+    if not exist "VS_Solution" (
+        mkdir "VS_Solution"
+    )
+    
+    cd internal_build_tools
+    call generate_vs_solution_internal.bat %BuildMode%
+)
+
+if %Compiler% == ClangNinja (
+    if not exist "ClangNinja" (
+        mkdir "ClangNinja"
+    )
+    
+    cd internal_build_tools
+    call generate_clang_ninja_internal.bat %BuildMode%
+)
+
 cd ..
