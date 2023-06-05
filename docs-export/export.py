1bbd533126b7ab23480ad9893cc319140a1601e4
Truman Kilen <t@kilen.me>
2023-06-05 14:50:09 -0500

    Rework docs build workflow

---
+++ docs-export/export.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+import subprocess
+import os
+import shutil
+
+# change dir to repo root
+os.chdir(os.path.dirname(os.path.dirname(__file__)))
+
+template_repo = 'docs-repo-template'
+out_dir = 'docs-repo'
+versions_out_dir = os.path.join(out_dir, 'versions')
+
+# get all version tags (prefixed with 'v')
+tags = subprocess.check_output(['git', 'tag', '-l', '--sort=v:refname', 'v*']).decode("utf-8").splitlines()
+
+# clear and recreat out directory
+shutil.rmtree(out_dir, ignore_errors=True)
+shutil.copytree(template_repo, out_dir)
+os.mkdir(versions_out_dir)
+
+def export_version(ref, name):
+    docs_spec = f'{ref}:docs'
+
+    # old tags do not have a 'docs' directory so check and fail if it does not exist
+    try:
+        subprocess.check_output(['git', 'show', docs_spec], stderr=subprocess.DEVNULL)
+    except subprocess.CalledProcessError:
+        return False
+
+    version_out_dir = os.path.join(versions_out_dir, name)
+    os.mkdir(version_out_dir)
+
+    # export docs/ via 'git archive' to version src/
+    src_dir = os.path.join(version_out_dir, 'src')
+    os.mkdir(src_dir)
+    ps = subprocess.Popen(('git', 'archive', docs_spec), stdout=subprocess.PIPE)
+    subprocess.check_output(('tar', 'xvf', '-', '-C', src_dir), stdin=ps.stdout)
+    ps.wait()
+
+    # insert banner
+    readme_path = os.path.join(src_dir, 'README.md')
+    with open(readme_path, 'r') as file:
+        lines = file.readlines()
+    lines.insert(2, '\n> WARNING: This is the version of the site that contains changes from the 3.XDev branch. The API and features are subject to change at any time. If you are developing mods for UE4SS, you should use the [stable version](https://ue4ss-re.github.io/docs) of the site instead.\n')
+    with open(readme_path, 'w') as file:
+        file.writelines(lines)
+
+    # copy template files
+    shutil.copy(os.path.join('docs-export', 'book.toml'), version_out_dir)
+    shutil.copytree(os.path.join('docs-export', 'css'), os.path.join(version_out_dir, 'css'))
+
+    return True
+
+export_version('main', 'dev')
+
+found_release = False
+if tags:
+    found_release = export_version(tags[-1], 'release')
+
+if not found_release:
+    # if no existing release than just use tip of main branch to have something
+    export_version('main', 'release')
+
+for tag in tags:
+    export_version(tag, tag)
---
+++ docs-repo-template/README.md
@@ -0,0 +1,3 @@
+# Wiki
+
+https://ue4ss-re.github.io/docs/
