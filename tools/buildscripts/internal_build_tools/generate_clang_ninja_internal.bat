3a580cffe09784c2ae56ec7dfadf798ef1b00e5e
narknon <73571427+narknon@users.noreply.github.com>
2023-06-26 14:37:13 -0400

    fix path for clangninja build script

--- tools/buildscripts/internal_build_tools/generate_clang_ninja_internal.bat
+++ tools/buildscripts/internal_build_tools/generate_clang_ninja_internal.bat
@@ -4,5 +4,5 @@ IF NOT DEFINED %BuildMode (
 set BuildMode=Release
 )
 
-cd ../ClangNinja
+cd ../../../ClangNinja
 cmake -DCMAKE_BUILD_TYPE=%BuildMode% -DRC_FORCE_ALL_STATIC_LIBS= -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -G"Ninja" ..
