b47204894b88a37de6042a91d87305ee241d58a8
Yangff <yangff1@gmail.com>
2024-02-08 00:58:42 -0600

    some testing

--- UE4SS/src/EntryLinux.cpp
+++ UE4SS/src/EntryLinux.cpp
@@ -7,17 +7,19 @@
 #include "UE4SSProgram.hpp"
 #include "Platform.hpp"
 
+#define _GNU_SOURCE
 #include <dlfcn.h>
 #include <link.h>
+#include <elf.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <sys/mman.h>
 
 using namespace RC;
 
 pthread_t ue4ss_mainthread;
 
-// constrcutor
-void __attribute__((constructor)) UE4SS_Start()
+void UE4SS_Start()
 {
     // find libUE4SS.so path using dlfcn
     Dl_info dl_info;
@@ -26,14 +28,22 @@ void __attribute__((constructor)) UE4SS_Start()
         std::cerr << "Failed to find libUE4SS.so path" << std::endl;
         return;
     }
-    
+    #define EARLY_THROW_TEST
+    #ifdef EARLY_THROW_TEST
+    try {
+        throw std::runtime_error{"Hello from Here!"};
+    } catch (std::exception& e) {
+        fprintf(stderr, "%s\n", e.what());
+    }
+    #endif
     SystemStringType ue4sspath = to_generic_string(dl_info.dli_fname);
     auto program = new UE4SSProgram(ue4sspath, {});
+    
     // use pthread here
     pthread_create(&ue4ss_mainthread, nullptr, [](void *arg) -> void * {
         auto program = (UE4SSProgram *)arg;
         program->init();
-
+        fprintf(stderr, "inited in thread\n");
         if (auto e = program->get_error_object(); e->has_error())
         {
             // If the output system errored out then use printf_s as a fallback
@@ -51,10 +61,76 @@ void __attribute__((constructor)) UE4SS_Start()
     }, program);
 }
 
+static bool UE4SSInited = false;
+
+/*
+void __attribute__((constructor)) UE4SS_Init() {
+    // It may not init in correct order..
+    if (!UE4SSInited) {
+        printf("UE4SS_Init Called\n");
+       // UE4SS_Start();
+        printf("UE4SS_Start Returned\n");
+        UE4SSInited = true;
+    }
+}
+*/
+
+static int (*next_main)(int, char **, char **);
+static bool inited = false;
+int hooked_main(int argc, char **argv, char **envp) {
+    // delay the hook to make sure all objects are constructed
+    fprintf(stderr, "Running hooked main before actual main my pid = %d\n", getpid());
+    if (!inited) {
+        fprintf(stderr, "Do another hook?\n");
+        UE4SS_Start();
+        inited = true;
+    } else {
+        fprintf(stderr, "UE4SS already inited? \n");
+    }
+    return next_main(argc, argv, envp);
+}
+
+#define ALIGN_UP_PAGE(x) (( ((unsigned long)(x)) + 0xFFF) & ~0xFFF)
+#define ALIGN_DOWN_PAGE(x) (((unsigned long)(x)) & ~0xFFF)
+
+extern "C" {
+int  __libc_start_main(
+    int (*main)(int, char **, char **),
+    int argc,
+    char **argv,
+    int (*init)(int, char **, char **),
+    void (*fini)(void),
+    void (*rtld_fini)(void),
+    void *stack_end)
+{
+    next_main = main;
+    typeof(&__libc_start_main) orig = (typeof(&__libc_start_main))dlsym(RTLD_NEXT, "__libc_start_main");
+    // gxx fix
+    void *current___gxx_personality_v0 = dlsym(RTLD_NEXT, "__gxx_personality_v0");
+    Dl_info dl_info;
+    Elf64_Sym *sym;
+    dladdr1(current___gxx_personality_v0, &dl_info, (void**) &sym, RTLD_DL_SYMENT);
+    fprintf(stderr, "__gxx_personality_v0 found in %s @ %p\n", dl_info.dli_fname, current___gxx_personality_v0);
+    if (strstr(dl_info.dli_fname, "libsteam_api.so") != nullptr) {
+        fprintf(stderr, "libsteam_api shit detected\n");
+        mprotect((void*)ALIGN_DOWN_PAGE(sym), ALIGN_UP_PAGE(sym + 1) - ALIGN_DOWN_PAGE(sym), PROT_READ | PROT_WRITE);
+        sym->st_info = ELF64_ST_INFO(STB_LOCAL, ELF64_ST_TYPE(sym->st_info));
+        sym->st_other = STV_INTERNAL;
+    }
+    current___gxx_personality_v0 = dlsym(RTLD_DEFAULT, "__gxx_personality_v0");
+    fprintf(stderr, "Now the current___gxx_personality_v0 is %p\n", current___gxx_personality_v0);
+
+    return orig(hooked_main, argc, argv, init, fini, rtld_fini, stack_end);
+}
+}
+
 // destructor
 void __attribute__((destructor)) UE4SS_End()
 {
-    UE4SSProgram::static_cleanup();
+    if (UE4SSInited) {
+        // UE4SSProgram::static_cleanup();
+        UE4SSInited = false;
+    }
 }
 
 std::filesystem::path get_executable_path() {
