2f21e1379bc7ac5e5037187056eaed547596b730
Yangff <yangff1@gmail.com>
2024-02-16 03:12:04 -0600

    fix path

--- .github/package.sh
+++ .github/package.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir package
-cp build/Output/Game__Shipping__Linux/libUE4SS.so package
+cp build/Output/Game__Shipping__Linux/UE4SS/bin/libUE4SS.so package
 cp assets/UE4SS-settings.ini package
