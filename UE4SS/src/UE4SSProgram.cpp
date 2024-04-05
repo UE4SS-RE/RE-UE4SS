ed989dfad28ec4f2a3ddbda3ead7104df811163c
UE4SS <wildbrim@hotmail.com>
2024-04-05 07:54:44 +0200

    Replaced usage of 'SetDefaultDllDirectories'
    This was done to fix PalServer not working properly after an update.
    The usage of 'SetDefaultDllDirectories' was there to allow C++ mods that have dynamic dependencies to store those dependencies right next to main.dll instead of in the Win64 directory.
    
    This behavior remains intact, but we're using 'LoadLibraryEx' instead of 'LoadLibrary' to set the path specifically for main.dll instead of globally with 'SetDefaultDllDirectories'.
    
    See #452 for discussion around this problem.

--- UE4SS/src/UE4SSProgram.cpp
+++ UE4SS/src/UE4SSProgram.cpp
@@ -391,9 +391,7 @@ namespace RC
         m_game_path_and_exe_name = game_exe_path;
         m_object_dumper_output_directory = m_game_executable_directory;
 
-        // Allow loading of DLLs from mod folders
-        SetDefaultDllDirectories(LOAD_LIBRARY_SEARCH_DEFAULT_DIRS);
-        // Make sure game directory DLLs are also included
+        // Allow loading of DLLs from the game directory
         AddDllDirectory(game_exe_path.c_str());
 
         for (const auto& item : std::filesystem::directory_iterator(m_root_directory))
