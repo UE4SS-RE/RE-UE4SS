1dda177957af2006f503c393fed8e4534fa01085
Yangff <yangff1@gmail.com>
2024-04-16 20:15:22 -0500

    force link order on linux

--- deps/third-repo/packages/p/polyhook_2/xmake.lua
+++ deps/third-repo/packages/p/polyhook_2/xmake.lua
@@ -5,7 +5,6 @@ package("polyhook_2")
 
     on_install(function (package)
         local configs = {}
-
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=OFF")
         if is_plat("linux") then
@@ -15,6 +14,11 @@ package("polyhook_2")
             table.insert(configs, "-DZYDIS_LIBRARY=" .. package:dep("zydis"):installdir("libzydis.a"))
         end
         import("package.tools.cmake").install(package, configs, { packagedeps = { "zycore", "zydis" } })
-        
+        print(package:get("links"))
+        package:add("links", "PolyHook_2")
+        package:add("links", "asmjit")
+        package:add("links", "asmtk")
+        print(package:get("links"))
+        -- fuckyou()
     end)
 package_end()
