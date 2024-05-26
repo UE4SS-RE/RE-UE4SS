9027adbd2b12bf86620a83cad8a77825cc4ebc5f
Yangff <yangff1@gmail.com>
2024-05-26 04:59:24 -0500

    the clean way refuse to work, sad

--- tools/zig/zig-cc-rust.bat
+++ tools/zig/zig-cc-rust.bat
@@ -1,3 +1,7 @@
-:; exec zig cc -target x86_64-linux-gnu "$@"; exit $?
-@echo off
+#!/bin/bash
+# 2>nul || @goto :batch
+exec zig cc -target x86_64-linux-gnu "$@"
+goto :EOF
+
+:batch
 zig cc -target x86_64-linux-gnu %*
