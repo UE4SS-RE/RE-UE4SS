93c7ef7ce7ec95327c6d173ef7dd18791cbcf649
Yangff <yangff1@gmail.com>
2024-05-26 04:34:53 -0500

    test unified .bat proxy

--- tools/zig/zig-ar.bat
+++ tools/zig/zig-ar.bat
@@ -1,1 +1,7 @@
+#!/bin/bash
+# 2>nul || @goto :batch
+exec zig ar "$@"
+goto :EOF
+
+:batch
 zig ar %*
--- tools/zig/zig-c++.bat
+++ tools/zig/zig-c++.bat
@@ -1,1 +1,7 @@
+#!/bin/bash
+# 2>nul || @goto :batch
+exec zig c++ -target x86_64-linux-gnu "$@"
+goto :EOF
+
+:batch
 zig c++ -target x86_64-linux-gnu %*
