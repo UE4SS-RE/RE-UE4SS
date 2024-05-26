681c83e845d83d97636f5c2f895d9e7505fb91b7
Fangfei Yang <yangff1@gmail.com>
2024-05-26 04:41:11 -0500

    make both cmake and cargo happy

--- tools/zig/zig-ar.bat
+++ tools/zig/zig-ar.bat
@@ -1,7 +1,1 @@
-#!/bin/bash
-# 2>nul || @goto :batch
-exec zig ar "$@"
-goto :EOF
-
-:batch
 zig ar %*
--- tools/zig/zig-c++.bat
+++ tools/zig/zig-c++.bat
@@ -1,7 +1,1 @@
-#!/bin/bash
-# 2>nul || @goto :batch
-exec zig c++ -target x86_64-linux-gnu "$@"
-goto :EOF
-
-:batch
 zig c++ -target x86_64-linux-gnu %*
