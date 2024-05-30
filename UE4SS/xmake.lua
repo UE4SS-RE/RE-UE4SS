576e0515fc2fbddf3453edb29a176295b6a70e28
narknon <73571427+narknon@users.noreply.github.com>
2024-05-30 10:56:09 -0400

    Require glaze in UE4SS project xmake

--- UE4SS/xmake.lua
+++ UE4SS/xmake.lua
@@ -5,6 +5,7 @@ add_requires("ImGuiTextEdit v1.0", { debug = is_mode_debug(), configs = {runtime
 add_requires("IconFontCppHeaders v1.0", { debug = is_mode_debug(), configs = {runtimes = get_mode_runtimes()}})
 add_requires("glfw 3.3.9", { debug = is_mode_debug() , configs = {runtimes = get_mode_runtimes()}})
 add_requires("opengl", { debug = is_mode_debug(), configs = {runtimes = get_mode_runtimes()} })
+add_requires("glaze", { debug = is_mode_debug(), configs = {runtimes = get_mode_runtimes()} })
 
 option("ue4ssBetaIsStarted")
     set_default(true)
