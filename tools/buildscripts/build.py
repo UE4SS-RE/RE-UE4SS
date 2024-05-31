1b56adfd8a44e94b08d0d248fb8bec4114cac0e2
Buckminsterfullerene02 <bobby45900@gmail.com>
2024-05-31 14:58:03 +0100

    another refactor pass

--- tools/buildscripts/build.py
+++ tools/buildscripts/build.py
@@ -7,19 +7,12 @@ import subprocess
 import argparse
 from datetime import datetime
 
-class Packager:
-    """
-    "Release" refers to the zip that is distributed to the end users. It should be in the format UE4SS_<version>.zip and contains only the necessary files.
-    "Dev" refers to the zip that is used for development. It should be in the format zDEV-UE4SS_<version>.zip and contains all the files, pdbs and docs.
-    """
-
-    def __init__(self):
-        self.release_output = 'release'
-        self.staging_dev = os.path.join(self.release_output, 'StagingDev')
-        self.staging_release = os.path.join(self.release_output, 'StagingRelease')
-
-        shutil.rmtree(self.release_output, ignore_errors=True)
-        os.mkdir(self.release_output)
+class ReleaseHandler:
+    def __init__(self, is_dev_release, release_output='release'):
+        self.is_dev_release = is_dev_release
+        self.release_output = release_output
+        self.staging_dir = os.path.join(self.release_output, 'StagingDev') if self.is_dev_release else os.path.join(self.release_output, 'StagingRelease')
+        self.ue4ss_dir = os.path.join(self.staging_dir, 'ue4ss')
 
         # List of CPP Mods with flags indicating if they need a config folder and if they should be included in release builds
         self.cpp_mods = {
@@ -50,23 +43,27 @@ class Packager:
             'GUIUFunctionCaller': 0,
         }
 
-    def make_staging_dirs(self, is_dev_release: bool):
-        staging_dir = self.staging_dev if is_dev_release else self.staging_release
-        staging_dir = os.path.join(staging_dir, 'ue4ss')
-        shutil.copytree('assets', staging_dir)
-        shutil.copy('README.md', os.path.join(staging_dir, 'README.md'))
+    def make_staging_dirs(self):
+        shutil.copytree('assets', self.ue4ss_dir)
+        shutil.copy('README.md', os.path.join(self.ue4ss_dir, 'README.md'))
 
-        if not is_dev_release:
+        if not self.is_dev_release:
             for file in self.files_to_exclude_from_release:
-                path = os.path.join(staging_dir, file)
+                path = os.path.join(self.ue4ss_dir, file)
                 try:
                     os.remove(path)
                 except:
                     shutil.rmtree(path)
-            self.modify_settings(staging_dir, self.settings_to_modify_in_release)
+            self.modify_settings(self.settings_to_modify_in_release)
 
-    def modify_settings(self, staging_dir, settings_to_modify):
-        config_path = os.path.join(staging_dir, 'UE4SS-settings.ini')
+        ue4ss_dll_path, ue4ss_pdb_path, dwmapi_dll_path, cpp_mods_paths = self.scan_directories()
+        self.copy_cpp_mods(cpp_mods_paths)
+        self.modify_mods_txt() # can only run this after copy mods just in case we are missing mod dlls
+        self.copy_executables(dwmapi_dll_path, ue4ss_dll_path, ue4ss_pdb_path)
+        self.copy_docs()
+
+    def modify_settings(self, settings_to_modify):
+        config_path = os.path.join(self.ue4ss_dir, 'UE4SS-settings.ini')
         with open(config_path, mode='r', encoding='utf-8-sig') as file:
             content = file.read()
 
@@ -76,35 +73,14 @@ class Packager:
 
         with open(config_path, mode='w', encoding='utf-8-sig') as file:
             file.write(content)
-    
-    def package_release(self, is_dev_release: bool):
-        version = subprocess.check_output(['git', 'describe', '--tags']).decode('utf-8').strip()
-        if is_dev_release:
-            main_zip_name = f'zDEV-UE4SS_{version}'
-            staging_dir = self.staging_dev
-        else:
-            main_zip_name = f'UE4SS_{version}'
-            staging_dir = self.staging_release
 
-        ue4ss_dll_path, ue4ss_pdb_path, dwmapi_dll_path, cpp_mods_paths = self.scan_directories(is_dev_release)
-        ue4ss_dir = os.path.join(staging_dir, 'ue4ss')
-
-        self.copy_cpp_mods(ue4ss_dir, cpp_mods_paths, is_dev_release)
-        self.modify_mods_txt(ue4ss_dir, is_dev_release) # can only run this after copy mods just in case we are missing mod dlls
-        self.copy_executables(staging_dir, ue4ss_dir, dwmapi_dll_path, ue4ss_dll_path, ue4ss_pdb_path, is_dev_release)
-        self.copy_docs(ue4ss_dir, is_dev_release)
-
-        output = os.path.join(self.release_output, main_zip_name)
-        shutil.make_archive(output, 'zip', staging_dir)
-        print(f'Created package {output}.zip')
-
-    def scan_directories(self, is_dev_release):
+    def scan_directories(self):
         ue4ss_dll_path = ''
         ue4ss_pdb_path = ''
         dwmapi_dll_path = ''
-        cpp_mods_paths = {mod: '' for mod in self.cpp_mods if is_dev_release or self.cpp_mods[mod]['include_in_release']}
+        cpp_mods_paths = {mod: '' for mod in self.cpp_mods if self.is_dev_release or self.cpp_mods[mod]['include_in_release']}
         scan_start_dir = '.'
-        
+
         for root, _, files in os.walk(scan_start_dir):
             for file in files:
                 if file.lower() == "ue4ss.dll":
@@ -116,13 +92,13 @@ class Packager:
                 for mod_name in cpp_mods_paths:
                     if file.lower() == mod_name.lower() + '.dll':
                         cpp_mods_paths[mod_name] = os.path.join(root, file)
-        
+
         return ue4ss_dll_path, ue4ss_pdb_path, dwmapi_dll_path, cpp_mods_paths
 
-    def copy_cpp_mods(self, staging_dir, cpp_mods_paths, is_dev_release):
+    def copy_cpp_mods(self, cpp_mods_paths):
         for mod_name, dll_path in cpp_mods_paths.items():
             if dll_path:
-                mod_dir = os.path.join(staging_dir, 'Mods', mod_name, 'dlls')
+                mod_dir = os.path.join(self.ue4ss_dir, 'Mods', mod_name, 'dlls')
                 os.makedirs(mod_dir, exist_ok=True)
                 shutil.copy(dll_path, os.path.join(mod_dir, 'main.dll'))
             else:
@@ -130,22 +106,23 @@ class Packager:
                 self.cpp_mods.pop(mod_name)
 
         for mod_name, mod_info in self.cpp_mods.items():
-            if is_dev_release or mod_info['include_in_release']:
+            if self.is_dev_release or mod_info['include_in_release']:
                 if mod_info['create_config']:
-                    os.makedirs(os.path.join(staging_dir, 'Mods', mod_name, 'config'), exist_ok=True)
+                    os.makedirs(os.path.join(self.ue4ss_dir, 'Mods', mod_name, 'config'), exist_ok=True)
 
-    def modify_mods_txt(self, staging_dir, is_dev_release):
+    def modify_mods_txt(self):
         for mod_name, mod_info in self.cpp_mods.items():
             if not mod_info['include_in_release']:
                 self.mods_to_disable_in_release[mod_name] = 0
-        
-        mods_path = os.path.join(staging_dir, 'Mods/mods.txt')
+
+        mods_path = os.path.join(self.ue4ss_dir, 'Mods/mods.txt')
         with open(mods_path, mode='r', encoding='utf-8-sig') as file:
             content = file.read()
 
-        if self.cpp_mods: content = '\n'.join([f'{mod} : 1' for mod in self.cpp_mods]) + '\n' + content
+        if self.cpp_mods:
+            content = '\n'.join([f'{mod} : 1' for mod in self.cpp_mods]) + '\n' + content
 
-        if not is_dev_release:
+        if not self.is_dev_release:
             for key, value in self.mods_to_disable_in_release.items():
                 pattern = rf'(^{key}\s*:).*?$'
                 content = re.sub(pattern, rf'\1 {value}', content, flags=re.MULTILINE)
@@ -153,35 +130,60 @@ class Packager:
         with open(mods_path, mode='w', encoding='utf-8-sig') as file:
             file.write(content)
 
-    def copy_executables(self, staging_dir, ue4ss_dir, dwmapi_dll_path, ue4ss_dll_path, ue4ss_pdb_path, is_dev_release):
-        shutil.copy(dwmapi_dll_path, staging_dir)
-        shutil.copy(ue4ss_dll_path, ue4ss_dir)
-        if is_dev_release: shutil.copy(ue4ss_pdb_path, ue4ss_dir)
+    def copy_executables(self, dwmapi_dll_path, ue4ss_dll_path, ue4ss_pdb_path):
+        shutil.copy(dwmapi_dll_path, self.staging_dir)
+        shutil.copy(ue4ss_dll_path, self.ue4ss_dir)
+        if self.is_dev_release: shutil.copy(ue4ss_pdb_path, self.ue4ss_dir)
 
-    def copy_docs(self, ue4ss_dir, is_dev_release):
-        if is_dev_release and os.path.exists('docs'): shutil.copytree('docs', os.path.join(ue4ss_dir, 'Docs'))
+    def copy_docs(self):
+        if self.is_dev_release and os.path.exists('docs'):
+            shutil.copytree('docs', os.path.join(self.ue4ss_dir, 'Docs'))
+
+    def package_release(self):
+        version = subprocess.check_output(['git', 'describe', '--tags']).decode('utf-8').strip()
+        main_zip_name = f'zDEV-UE4SS_{version}' if self.is_dev_release else f'UE4SS_{version}'
+        output = os.path.join(self.release_output, main_zip_name)
+        shutil.make_archive(output, 'zip', self.staging_dir)
+        print(f'Created package {output}.zip')
+
+    def cleanup(self):
+        shutil.rmtree(self.staging_dir, ignore_errors=True)
+
+class Packager:
+    """
+    "Release" refers to the zip that is distributed to the end users. It should be in the format UE4SS_<version>.zip and contains only the necessary files.
+    "Dev" refers to the zip that is used for development. It should be in the format zDEV-UE4SS_<version>.zip and contains all the files, pdbs and docs.
+    """
+
+    def __init__(self, args):
+        self.args = args
+        self.release_output = 'release'
+        self.setup()
+
+    def setup(self):
+        shutil.rmtree(self.release_output, ignore_errors=True)
+        os.mkdir(self.release_output)
 
     def run(self):
-        self.make_staging_dirs(is_dev_release=True)
-        self.make_staging_dirs(is_dev_release=False)
+        dev_handler = ReleaseHandler(is_dev_release=True, release_output=self.release_output)
+        release_handler = ReleaseHandler(is_dev_release=False, release_output=self.release_output)
 
-        self.package_release(is_dev_release=True)
-        self.package_release(is_dev_release=False)
+        dev_handler.make_staging_dirs()
+        dev_handler.package_release()
+        dev_handler.cleanup()
+
+        release_handler.make_staging_dirs()
+        release_handler.package_release()
+        release_handler.cleanup()
 
         shutil.make_archive(os.path.join(self.release_output, 'zCustomGameConfigs'), 'zip', 'assets/CustomGameConfigs')
         shutil.make_archive(os.path.join(self.release_output, 'zMapGenBP'), 'zip', 'assets/MapGenBP')
-
-        self.cleanup()
 
         changelog = parse_changelog()
         with open(os.path.join(self.release_output, 'release_notes.md'), 'w') as file:
             file.write(changelog[0]['notes'])
 
         print('Done')
-
-    def cleanup(self):
-        shutil.rmtree(self.staging_dev, ignore_errors=True)
-        shutil.rmtree(self.staging_release, ignore_errors=True)
 
 changelog_path = 'assets/Changelog.md'
 
