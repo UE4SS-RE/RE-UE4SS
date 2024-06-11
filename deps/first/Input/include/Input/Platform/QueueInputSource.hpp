ff47e452f43cc88f00c6a2da204d48e8c002c8d2
Yangff <yangff1@gmail.com>
2024-06-11 00:10:01 -0500

    Input Source: GLFW3; Win32 Async; Common Queued Source

---
+++ deps/first/Input/include/Input/Platform/QueueInputSource.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <Input/PlatformInputSource.hpp>
+#include <Input/KeyDef.hpp>
+
+#include <Input/RingBuffer.hpp>
+
+namespace RC::Input
+{
+    class QueueInputSource : public PlatformInputSource
+    {
+      private:
+        static constexpr int max_inputs = 256;
+        RingBufferSPSC<InputEvent, max_inputs> m_input_queue;
+
+      protected:
+        bool m_activated{false};
+
+      private:
+        /// SAFETY: Only update and return m_input_events
+        /// in process_event and flush_events functions
+        std::vector<InputEvent> m_input_events;
+
+      public:
+        ~QueueInputSource() = default;
+
+        // QueeueInputSource is not a implemented input source
+        // and should not be used directly
+        bool is_available() override
+        {
+            return false;
+        };
+
+        bool activate() override
+        {
+            return m_activated = true;
+        };
+
+        bool deactivate() override
+        {
+            m_activated = false;
+            return true;
+        };
+
+        std::vector<InputEvent>& process_event(Handler* handler) override;
+
+        int source_priority() override
+        {
+            return 0;
+        }
+
+        const char* get_name() override
+        {
+            return "Queue";
+        }
+
+      public:
+        auto push_input_event(const InputEvent& event) -> void;
+    };
+
+}; // namespace RC::Input
