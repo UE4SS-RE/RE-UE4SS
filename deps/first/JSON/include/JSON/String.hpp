d681438fae9cf55ddfd7dd8547d4ba4b959793a1
narknon <73571427+narknon@users.noreply.github.com>
2024-06-15 15:34:50 -0400

    Add Deprecation tags to JSON Module

--- deps/first/JSON/include/JSON/String.hpp
+++ deps/first/JSON/include/JSON/String.hpp
@@ -32,8 +32,8 @@ namespace RC::JSON
         }
 
       public:
-        RC_JSON_API auto serialize(ShouldFormat should_format = ShouldFormat::No, int32_t* indent_level = nullptr) -> StringType override;
-        RC_JSON_API auto get_type() const -> Type override
+        RC_JSON_API JSON_DEPRECATED auto serialize(ShouldFormat should_format = ShouldFormat::No, int32_t* indent_level = nullptr) -> StringType override;
+        RC_JSON_API JSON_DEPRECATED auto get_type() const -> Type override
         {
             return Type::String;
         }
