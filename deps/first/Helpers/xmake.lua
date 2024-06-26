4dc92d29bf3517296a1f637eb5ab287be942f1b3
localcc <work@localcc.cc>
2024-06-26 14:47:54 +0200

    chore(Helpers): remove windows.h leaking

--- deps/first/Helpers/xmake.lua
+++ deps/first/Helpers/xmake.lua
@@ -1,10 +1,12 @@
 local projectName = "Helpers"
 
 target(projectName)
-    set_kind("headeronly")
+    set_kind("static")
     set_languages("cxx20")
     set_exceptions("cxx")
     add_rules("ue4ss.dependency")
 
     add_includedirs("include", { public = true })
     add_headerfiles("include/**.hpp")
+
+    add_files("src/**.cpp")
