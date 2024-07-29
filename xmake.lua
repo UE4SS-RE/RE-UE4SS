86c09a724806609019c4f8af087c452bb3044e56
UE4SS <91646798+UE4SS@users.noreply.github.com>
2024-07-29 20:36:37 +0200

    fix(CI): Locked glaze to 2.9.5, and we now require xmake >=2.9.3 (#603)
    * fix(CI): Locked glaze to 2.9.5, and we now require xmake >=2.9.3
    
    * fix(xmake): Bump requirement to >=2.9.3
    
    * fix(CI): Lock xmake to version 2.9.3

--- xmake.lua
+++ xmake.lua
@@ -1,4 +1,4 @@
-set_xmakever("2.9.2")
+set_xmakever("2.9.3")
 -- We should use `get_config("ue4ssRoot")` instead of `os.projectdir()` or `$(projectdir)`.
 -- This is because os.projectdir() will return a higher parent dir
 -- when UE4SS is sub-moduled/`include("UE4SS")` in another xmake project.
