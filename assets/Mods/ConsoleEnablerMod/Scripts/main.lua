1e0ab03c310f06f876b7b4641b3ed6e330cd3482
igromanru <igromanru@yahoo.de>
2024-10-17 14:51:54 +0200

    feat: Check if F10 is already used as Console key and add it as new key instead of replacing the first one feat: Add some missing IsValid checks refactor: Removed obsolete parameters

--- assets/Mods/ConsoleEnablerMod/Scripts/main.lua
+++ assets/Mods/ConsoleEnablerMod/Scripts/main.lua
@@ -1,42 +1,52 @@
-local Engine = FindFirstOf("Engine")
+local UEHelpers = require("UEHelpers")
+
 local PlayerControllerHookActive = false
 local WasFirstConsoleCreated = false
 
 local function RemapConsoleKeys()
     -- Change console key
-    local InputSettings = StaticFindObject("/Script/Engine.Default__InputSettings")
+    local InputSettings = StaticFindObject("/Script/Engine.Default__InputSettings") ---@cast InputSettings UInputSettings
     if not InputSettings:IsValid() then print("[ConsoleEnabler] InputSettings not found, could not change console key\n") return end
-    
+
     local ConsoleKeys = InputSettings.ConsoleKeys
-    
-    -- This sets the first console key to F10
-    ConsoleKeys[1].KeyName = FName("F10", EFindName.FNAME_Find)
-    
-    ConsoleKeys:ForEach(function(index, elem_wrapper)
-        local KeyStruct = elem_wrapper:get()
-        local KeyFName = KeyStruct.KeyName
-        -- The ToString() call here will go bad if the FName is not ansi
-        print(string.format("[ConsoleEnabler] ConsoleKey[%d]: %s\n", index, KeyFName:ToString()))
-    end)
+
+    local f10Name = UEHelpers.FindFName("F10")
+    if f10Name == NAME_None then
+        print("[RemapConsoleKeys] Was unable to find F10 FName\n")
+        return
+    end
+
+    local f10AlreadySet = false
+    for i = 1, #ConsoleKeys do
+        local consoleKey = ConsoleKeys[i]
+        local keyName = consoleKey.KeyName
+        if keyName == f10Name then
+            f10AlreadySet = true
+        end
+        print(string.format("[ConsoleEnabler] ConsoleKey[%d]: %s\n", i, keyName:ToString()))
+    end
+    if not f10AlreadySet then
+        local newKeyIndex = #ConsoleKeys + 1
+        ConsoleKeys[newKeyIndex].KeyName = f10Name
+        print(string.format("[ConsoleEnabler] ConsoleKey[%d]: %s\n", newKeyIndex, f10Name:ToString()))
+    end
 end
 
 local function CreateConsole()
-    if not Engine:IsValid() then
-        Engine = FindFirstOf("Engine")
-    end
-    
+    local Engine = UEHelpers.GetEngine()
     if not Engine:IsValid() then print("[ConsoleEnabler] Was unable to find an instance of UEngine\n") return end
-    
-    local ConsoleClass = Engine.ConsoleClass
+
+    local ConsoleClass = Engine.ConsoleClass ---@type UClass
     local GameViewport = Engine.GameViewport
-    
-    if GameViewport.ViewportConsole:IsValid() then
+
+    if GameViewport:IsValid() and GameViewport.ViewportConsole:IsValid() then
         -- Console already exists, let's just remap the keys
         RemapConsoleKeys()
     elseif ConsoleClass:IsValid() and GameViewport:IsValid() then
-        local CreatedConsole = StaticConstructObject(ConsoleClass, GameViewport, 0, 0, 0, nil, false, false, nil)
+        local CreatedConsole = StaticConstructObject(ConsoleClass, GameViewport) ---@cast CreatedConsole UConsole
+        if not CreatedConsole:IsValid() then print("[CreateConsole] Was unable to construct an UConsole object\n") return end
+
         GameViewport.ViewportConsole = CreatedConsole
-        
         PlayerControllerHookActive = true
         WasFirstConsoleCreated = true
 
@@ -48,7 +58,7 @@ end
 
 CreateConsole()
 
-NotifyOnNewObject("/Script/Engine.PlayerController", function(CreatedObject)
+NotifyOnNewObject("/Script/Engine.PlayerController", function()
     if PlayerControllerHookActive or not WasFirstConsoleCreated then
         CreateConsole()
     end
