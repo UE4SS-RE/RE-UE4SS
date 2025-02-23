b3a62e62ff7ef076d9626f1d4956040e752e2a5e
Igromanru <igromanru@yahoo.de>
2025-02-23 14:21:18 +0100

    feat: Add Custom Game Configs for Lies of P (#787)

---
+++ assets/CustomGameConfigs/Lies of P/UE4SS_Signatures/FText_Constructor.lua
@@ -0,0 +1,7 @@
+function Register()
+    return "48 89 5C 24 ?? 48 89 6C 24 ?? 56 57 41 54 41 56 41 57 48 83 EC ?? 45 33 E4 48 8B F1 48 8B 0D"
+end
+
+function OnMatchFound(MatchAddress)
+    return MatchAddress
+end
