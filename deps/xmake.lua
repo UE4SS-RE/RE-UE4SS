dd884143b37a2240f0e0a3104cddae597b735fcc
narknon <73571427+narknon@users.noreply.github.com>
2025-05-23 15:22:09 -0400

    chore(sub): update zydis and zycore

--- deps/xmake.lua
+++ deps/xmake.lua
@@ -6,6 +6,6 @@ includes("third")
 -- Everything that is an xmake package should be inside of this repository
 add_repositories("third-party deps/third-repo", { rootdir = get_config("ue4ssRoot") })
 
-add_requires("zycore v1.5.0", { debug = is_mode_debug(), configs = {runtimes = get_mode_runtimes()} })
-add_requires("zydis v4.1.0", { debug = is_mode_debug(), configs = {runtimes = get_mode_runtimes()} })
+add_requires("zycore v1.5.1", { debug = is_mode_debug(), configs = {runtimes = get_mode_runtimes()} })
+add_requires("zydis v4.1.1", { debug = is_mode_debug(), configs = {runtimes = get_mode_runtimes()} })
 add_requires("polyhook_2", { debug = is_mode_debug(), configs = {runtimes = get_mode_runtimes()} })
