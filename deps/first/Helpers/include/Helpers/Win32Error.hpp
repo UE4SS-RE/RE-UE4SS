474a6ef331556f454d083c5b52fc9333b9fc76f4
Xebeth <github@north-edge.com>
2025-06-05 18:10:32 +0100

    Rename win32_error to Win32Error and refactor following feedback

--- deps/first/Helpers/include/Helpers/Win32Error.hpp
+++ deps/first/Helpers/include/Helpers/Win32Error.hpp
@@ -2,76 +2,24 @@
 
 #ifdef _WIN32
 
-#include <windows.h>
-#include <format>
-
 namespace RC
 {
-    template<typename char_t>
-    class win32_error
+    class Win32Error
     {
     public:
-        explicit win32_error(DWORD errorCode, DWORD dwLanguageId = MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT))
-            : m_pText(nullptr)
-        {
-            assign(errorCode, dwLanguageId);
-        }
+        explicit Win32Error(DWORD errorCode, const DWORD* dwLanguageId = nullptr);
+        explicit Win32Error(HRESULT hResult, const DWORD* dwLanguageId = nullptr);
 
-        explicit win32_error(HRESULT hResult, DWORD dwLanguageId = MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT))
-            : m_pText(nullptr)
-        {
-            assign(hResult, dwLanguageId);
-        }
+        auto assign(DWORD errorCode, DWORD dwLanguageId) -> void;
+        auto assign(HRESULT hResult, DWORD dwLanguageId) -> void;
 
-        ~win32_error()
-        {
-            clear();
-        }
+        [[nodiscard]] auto c_str() const -> const CharType* { return m_ErrorText.c_str(); }
 
-        auto clear() -> void
-        {
-            if (m_pText != nullptr)
-            {
-                ::LocalFree(m_pText);
-                m_pText = nullptr;
-            }
-        }
-
-        auto assign(DWORD errorCode, DWORD dwLanguageId) -> const char_t*
-        {
-            clear();
-            m_pText = format_error(errorCode, dwLanguageId);
-
-            if (m_pText == nullptr)
-            {
-                wprintf(L"Failed to format the error message for code 0x%08lX: error 0x%08lX\n", errorCode, GetLastError());
-                m_pText = static_cast<char_t*>(LocalAlloc(0, 1));
-                *m_pText = '\0';
-            }
-
-            return m_pText;
-        }
-
-        auto assign(HRESULT hResult, DWORD dwLanguageId) -> void
-        {
-            if (HRESULT_FACILITY(hResult) == FACILITY_WIN32)
-            {
-                assign(static_cast<DWORD>(hResult), dwLanguageId);
-            }
-            else
-            {
-                wprintf(L"Failed to format the error message: HRESULT 0x%08lX is not a WIN32 error code\n", hResult);
-                m_pText = static_cast<char_t*>(LocalAlloc(0, 1));
-                *m_pText = '\0';
-            }
-        }
-
-        [[nodiscard]] auto c_str() const -> char_t* { return m_pText; }
-        explicit operator char_t*() const { return c_str(); }
+        explicit operator const CharType*() const { return c_str(); } // must be explicit, or it will cause an ambiguous call to overloaded function error when passed to RC::to_string
+        operator const StringType&() const { return m_ErrorText; } // implicit cast so it can be passed to RC::to_string
     private:
-        static auto format_error(DWORD errorCode, DWORD dwLanguageId) -> char_t*;
-
-        char_t* m_pText;
+        static auto format_error(DWORD errorCode, DWORD dwLanguageId) -> CharType*;
+        StringType m_ErrorText;
     };
 }
 
--- deps/first/Helpers/src/Win32Error.cpp
+++ deps/first/Helpers/src/Win32Error.cpp
@@ -1,31 +1,62 @@
 #ifdef _WIN32
+#include <windows.h>
+#include <format>
 
-#include <Helpers/Win32Error.hpp>
+#include <String/StringType.hpp>
+
+#include "Helpers/Win32Error.hpp"
 
 namespace RC
 {
-    template<> auto win32_error<wchar_t>::format_error(DWORD errorCode, DWORD dwLanguageId) -> wchar_t*
+    Win32Error::Win32Error(DWORD errorCode, const DWORD *dwLanguageId)
     {
-        LPVOID pBuffer = nullptr;
-        DWORD dwFlags = FORMAT_MESSAGE_FROM_SYSTEM
-                      | FORMAT_MESSAGE_ALLOCATE_BUFFER
-                      | FORMAT_MESSAGE_IGNORE_INSERTS
-                      | FORMAT_MESSAGE_MAX_WIDTH_MASK;
+        const auto langId = dwLanguageId == nullptr ? MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT) : *dwLanguageId;
 
-        return FormatMessageW(dwFlags, nullptr, errorCode, dwLanguageId, reinterpret_cast<wchar_t*>(&pBuffer), 0, nullptr) == 0
-             ? nullptr : static_cast<wchar_t*>(pBuffer);
+        assign(errorCode, langId);
     }
 
-    template<> auto win32_error<char>::format_error(DWORD errorCode, DWORD dwLanguageId) -> char*
+    Win32Error::Win32Error(HRESULT hResult, const DWORD *dwLanguageId)
     {
-        LPVOID pBuffer = nullptr;
+        const auto langId = dwLanguageId == nullptr ? MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT) : *dwLanguageId;
+
+        assign(hResult, langId);
+    }
+
+    auto Win32Error::assign(DWORD errorCode, DWORD dwLanguageId) -> void
+    {
+        if (auto pErrorText = format_error(errorCode, dwLanguageId); pErrorText != nullptr)
+        {
+            m_ErrorText = std::format(L"[0x{:x}] {}", errorCode, pErrorText);
+            LocalFree(pErrorText);
+        }
+        else
+        {
+            m_ErrorText = std::format(L"Failed to format the error message for code {:x}: error {:x}\n", errorCode, GetLastError());
+        }
+    }
+
+    auto Win32Error::assign(HRESULT hResult, DWORD dwLanguageId) -> void
+    {
+        if (HRESULT_FACILITY(hResult) == FACILITY_WIN32)
+        {
+            assign(static_cast<DWORD>(hResult), dwLanguageId);
+        }
+        else
+        {
+           m_ErrorText = std::format(L"Failed to format the error message: HRESULT 0x%08lX is not a WIN32 error code\n", hResult);
+        }
+    }
+
+    auto Win32Error::format_error(DWORD errorCode, DWORD dwLanguageId) -> CharType*
+    {
         DWORD dwFlags = FORMAT_MESSAGE_FROM_SYSTEM
                       | FORMAT_MESSAGE_ALLOCATE_BUFFER
                       | FORMAT_MESSAGE_IGNORE_INSERTS
                       | FORMAT_MESSAGE_MAX_WIDTH_MASK;
+        LPVOID pBuffer = nullptr;
 
-        return FormatMessageA(dwFlags, nullptr, errorCode, dwLanguageId, reinterpret_cast<char*>(&pBuffer), 0, nullptr) == 0
-             ? nullptr : static_cast<char*>(pBuffer);
+        return FormatMessage(dwFlags, nullptr, errorCode, dwLanguageId, reinterpret_cast<CharType*>(&pBuffer), 0, nullptr) == 0
+             ? nullptr : static_cast<CharType*>(pBuffer);
     }
 }
 
