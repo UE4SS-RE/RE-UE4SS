8e08d135cba514f53591a5cc3026a5c4c66a0c33
Kein <kein-of@yandex.ru>
2025-06-05 23:32:42 +0300

    Avoid re-creating CM if it already exist
    Preserves original game functionality

--- assets/Mods/CheatManagerEnablerMod/Scripts/main.lua
+++ assets/Mods/CheatManagerEnablerMod/Scripts/main.lua
@@ -1,25 +1,32 @@
+---@param self RemoteUnrealParam<APlayerController>
+---@param NewPawn RemoteUnrealParam<APawn>
 RegisterHook("/Script/Engine.PlayerController:ClientRestart", function(self, NewPawn)
     local PlayerController = self:get()
-    
-    local CheatManagerClass = PlayerController.CheatClass
-    if not CheatManagerClass:IsValid() then
-        print("[CheatManager Creator] Controller:CheatClass is nullptr, using default CheatClass instead\n")
-        
-        CheatManagerClass = StaticFindObject("/Script/Engine.CheatManager")
-    end
-    
-    if not CheatManagerClass:IsValid() then
-        print("[CheatManager Creator] Couldn't find default CheatClass, therefore, could not enable Cheat Manager\n")
+
+    if PlayerController.CheatManager:IsValid() then
+        print("[CheatManagerEnabler] CheatManager already exist, skipping restoration\n")
         return
     end
-    
-    local CreatedCheatManager = StaticConstructObject(CheatManagerClass, PlayerController, 0, 0, 0, nil, false, false, nil)
+
+    local CheatManagerClass = PlayerController.CheatClass
+    if not CheatManagerClass:IsValid() then
+        print("[CheatManagerEnabler] Controller:CheatClass is nullptr, using default CheatClass instead\n")
+
+        CheatManagerClass = StaticFindObject("/Script/Engine.CheatManager") --[[@as UClass]]
+    end
+
+    if not CheatManagerClass:IsValid() then
+        print("[CheatManagerEnabler] Couldn't find default CheatClass, therefore, could not enable Cheat Manager\n")
+        return
+    end
+
+    local CreatedCheatManager = StaticConstructObject(CheatManagerClass, PlayerController)
     if CreatedCheatManager:IsValid() then
-        print(string.format("[CheatManager Creator] Constructed CheatManager [0x%X]\n", CreatedCheatManager:GetAddress()))
-        
+        print(string.format("[CheatManagerEnabler] Constructed CheatManager [0x%X]\n", CreatedCheatManager:GetAddress()))
+
         PlayerController.CheatManager = CreatedCheatManager
-        print("[CheatManager Creator] Enabled CheatManager\n")
+        print("[CheatManagerEnabler] Enabled CheatManager\n")
     else
-        print("[CheatManager Creator] Was unable to construct CheatManager, therefore, could not enable Cheat Manager\n")
+        print("[CheatManagerEnabler] Was unable to construct CheatManager, therefore, could not enable Cheat Manager\n")
     end
 end)
