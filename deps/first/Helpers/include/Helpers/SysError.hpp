0f55683b3963e3e25e5d1b5a0b2c475518b0a14a
Xebeth <github@north-edge.com>
2025-06-12 17:57:36 +0100

    Fix casing of variables and members

--- deps/first/Helpers/include/Helpers/SysError.hpp
+++ deps/first/Helpers/include/Helpers/SysError.hpp
@@ -4,6 +4,10 @@
 
 #ifdef _WIN32
     #define DEFAULT_ERROR_CATEGORY std::system_category()
+
+    #ifndef HRESULT
+        #define HRESULT long
+    #endif
 #else
     #define DEFAULT_ERROR_CATEGORY std::generic_category()
 #endif
@@ -18,39 +22,39 @@ namespace RC
     public:
         /**
          * Constructor for error code
-         * @param errorCode : a system error code
+         * @param error_code : a system error code
          * @param category : the error category (std::system_category() for OS specific codes or std::generic_category() for POSIX codes)
          */
-        explicit SysError(int errorCode, const std::error_category &category = DEFAULT_ERROR_CATEGORY);
+        explicit SysError(int error_code, const std::error_category& category = DEFAULT_ERROR_CATEGORY);
         /**
          * Constructor for error code
-         * @param errorCode : a system error code
+         * @param error_code : a system error code
         * @param category : the error category (std::system_category() for OS specific codes or std::generic_category() for POSIX codes)
          */
-        explicit SysError(unsigned long errorCode, const std::error_category &category = DEFAULT_ERROR_CATEGORY);
+        explicit SysError(unsigned long error_code, const std::error_category& category = DEFAULT_ERROR_CATEGORY);
         /**
          * Assign an error code and update the object with the corresponding error message
-         * @param errorCode : a system error code
+         * @param error_code : a system error code
          */
-        auto assign(unsigned long errorCode) -> void;
+        auto assign(unsigned long error_code) -> void;
 #ifdef _WIN32
         /**
          * Constructor for COM HRESULT codes -- only works for FACILITY_WIN32
-         * @param hResult : a COM operation result code
+         * @param hresult : a COM operation result code
          */
-        explicit SysError(HRESULT hResult);
+        explicit SysError(HRESULT hresult);
 
         /**
          * Assign an error code and update the object with the corresponding error message
-         * @param hResult : a COM operation result code
+         * @param hresult : a COM operation result code
          */
-        auto assign(HRESULT hResult) -> void;
+        auto assign(HRESULT hresult) -> void;
 #endif
         /**
          * Returns a pointer to an array that contains a null-terminated sequence of characters representing the current value of the string object
          * @return a pointer to the c-string representation of the string object's value
          */
-        [[nodiscard]] auto c_str() const -> const CharType* { return m_ErrorText.c_str(); }
+        [[nodiscard]] auto c_str() const -> const CharType* { return m_error_text.c_str(); }
         /**
          * Returns the name of the error category
          * @return the name of the error category
@@ -63,21 +67,21 @@ namespace RC
         /**
          * Implicit cast operator to const StringType& so that the wrapper can be passed to RC::to_string, for instance
          */
-        operator const StringType&() const { return m_ErrorText; }
-        /**
-         * The error category (std::system_category() for OS specific codes or std::generic_category() for POSIX codes)
-         */
-        const std::error_category* m_pErrorCategory = nullptr;
+        operator const StringType&() const { return m_error_text; }
     private:
         /**
          * Formats the error message corresponding to the specified error code.
-         * @param errorCode : a system error code or COM error code
+         * @param error_code : a system error code or COM error code
          * @return a string containing the formatted message, if successful
          */
-        auto format_error(int errorCode) const -> StringType;
+        [[nodiscard]] auto format_error(int error_code) const -> StringType;
+        /**
+         * The error category (std::system_category() for OS specific codes or std::generic_category() for POSIX codes)
+         */
+        const std::error_category* m_error_category = nullptr;
         /**
          * The string object backing the wrapper
          */
-        StringType m_ErrorText;
+        StringType m_error_text;
     };
 }
--- deps/first/Helpers/src/SysError.cpp
+++ deps/first/Helpers/src/SysError.cpp
@@ -14,57 +14,57 @@
 
 namespace RC
 {
-    SysError::SysError(const int errorCode, const std::error_category &category)
-        : m_pErrorCategory(&category)
+    SysError::SysError(const int error_code, const std::error_category &category)
+        : m_error_category(&category)
     {
-        assign(static_cast<unsigned long>(errorCode));
+        assign(static_cast<unsigned long>(error_code));
     }
 
-    SysError::SysError(const unsigned long errorCode, const std::error_category &category)
-        : m_pErrorCategory(&category)
+    SysError::SysError(const unsigned long error_code, const std::error_category& category)
+        : m_error_category(&category)
     {
-        assign(errorCode);
+        assign(error_code);
     }
 
-    auto SysError::assign(unsigned long errorCode) -> void
+    auto SysError::assign(unsigned long error_code) -> void
     {
-        m_ErrorText.assign(std::format(L"[0x{:x}] {}", errorCode, format_error(static_cast<int>(errorCode))));
+        m_error_text.assign(std::format(L"[0x{:x}] {}", error_code, format_error(static_cast<int>(error_code))));
     }
 
 #ifdef _WIN32
-    SysError::SysError(HRESULT hResult): m_pErrorCategory(&std::system_category())
+    SysError::SysError(const HRESULT hresult): m_error_category(&std::system_category())
     {
-        assign(hResult);
+        assign(hresult);
     }
 
-    auto SysError::assign(HRESULT hResult) -> void
+    auto SysError::assign(HRESULT hresult) -> void
     {
-        if (HRESULT_FACILITY(hResult) == FACILITY_WIN32)
+        if (HRESULT_FACILITY(hresult) == FACILITY_WIN32)
         {
-            assign(static_cast<unsigned long>(hResult));
+            assign(static_cast<unsigned long>(hresult));
         }
         else
         {
            // this error code will most likely be formatted to an unrelated error message, inform the caller
-           m_ErrorText.assign(std::format(L"Failed to format the error message: HRESULT 0x{:x} is not a WIN32 error code", hResult));
+           m_error_text.assign(std::format(L"Failed to format the error message: HRESULT 0x{:x} is not a WIN32 error code", hresult));
         }
     }
 #endif
 
     auto SysError::category() const -> StringType
     {
-        return to_wstring(m_pErrorCategory ? m_pErrorCategory->name() : DEFAULT_ERROR_CATEGORY.name());
+        return to_wstring(m_error_category ? m_error_category->name() : DEFAULT_ERROR_CATEGORY.name());
     }
 
-    auto SysError::format_error(const int errorCode) const -> StringType
+    auto SysError::format_error(const int error_code) const -> StringType
     {
-        const std::error_category& errorCategory = m_pErrorCategory ? *m_pErrorCategory : DEFAULT_ERROR_CATEGORY;
-        const std::error_code ec(errorCode, errorCategory);
+        const std::error_category& error_category = m_error_category ? *m_error_category : DEFAULT_ERROR_CATEGORY;
+        const std::error_code ec(error_code, error_category);
         // remove new line(s) and tabs
         auto result = std::regex_replace(to_wstring(std::system_error(ec).what()), std::wregex(STR("(\t|\r?\n)")), STR(" "));
         // right trim
-        result.erase(std::ranges::find_if(std::ranges::reverse_view(result), [](const CharType ch) -> bool {
-            return !std::isspace<CharType>(ch, std::locale::classic());
+        result.erase(std::ranges::find_if(std::ranges::reverse_view(result), [](const CharType c) -> bool {
+            return !std::isspace<CharType>(c, std::locale::classic());
         }).base(), result.end());
 
         return result;
