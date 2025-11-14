35c6c45aa9e7dc9fb531ee776ee3ac059083cb5c
UE4SS <wildbrim@hotmail.com>
2025-11-14 07:52:51 +0100

    feat(LiveView): Highlighting for properties filters
    Specifically for properties set in the 'Has property' and 'Has property of type' filters.
    
    This commit also fixes properties in inherited classes from being included.
    We only ever care about properties from the SuperStruct linked list, not the ClassPrivate linked list.
    This is a bug in the Unreal module that hasn't yet been fixed, so we have to compensate here.

--- UE4SS/include/GUI/LiveView/Filter/HasProperty.hpp
+++ UE4SS/include/GUI/LiveView/Filter/HasProperty.hpp
@@ -14,10 +14,44 @@ namespace RC::GUI::Filter
 
         static auto post_eval(UObject* object) -> bool
         {
-            for (const auto& property : list_properties)
+            for (const auto& property_string : list_properties)
             {
-                if (!property.empty() && !object->GetPropertyByNameInChain(FromCharTypePtr<TCHAR>(property.c_str()))) return true;
+                if (property_string.empty())
+                {
+                    continue;
+                }
+                const auto property_name = FName(FromCharTypePtr<TCHAR>(property_string.c_str()));
+                const auto as_struct = object->IsA<UStruct>() ? static_cast<UStruct*>(object) : object->GetClassPrivate();
+                FProperty* found_property{};
+                for (FProperty* property : as_struct->ForEachProperty())
+                {
+                    if (property->GetFName().Equals(property_name))
+                    {
+                        found_property = property;
+                        break;
+                    }
+                }
+                for (UStruct* super_struct : as_struct->ForEachSuperStruct())
+                {
+                    for (FProperty* property : super_struct->ForEachProperty())
+                    {
+                        if (property->GetFName().Equals(property_name))
+                        {
+                            found_property = property;
+                            break;
+                        }
+                    }
+                }
+                if (!found_property)
+                {
+                    return true;
+                }
+                else
+                {
+                    highlight(found_property);
+                }
             }
+
             return false;
         }
     };
