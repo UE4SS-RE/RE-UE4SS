6be36d8a7ab91f88ffb8c94d20cd137258ca2449
UE4SS <wildbrim@hotmail.com>
2025-12-04 16:08:53 +0100

    chore: Vein custom config updated for Stable beta Steam branch

--- assets/CustomGameConfigs/Vein/UE4SS_Signatures/ProcessLocalScriptFunction.lua
+++
@@ -1,7 +0,0 @@
-function Register()
-    return "40 55 53 56 57 41 55 41 56 48 8D AC 24 A8 FE FF FF 48 81 EC 58 02 00 00 48 8B ?? ?? ?? ?? ?? 48 33 C4 48 89 85 40 01 00 00 48 8B 7A 10 48 8D 44 24 40"
-end
-
-function OnMatchFound(MatchAddress)
-    return MatchAddress
-end
--- assets/CustomGameConfigs/Vein/UE4SS_Signatures/StaticConstructObject.lua
+++ assets/CustomGameConfigs/Vein/UE4SS_Signatures/StaticConstructObject.lua
@@ -1,5 +1,5 @@
 function Register()
-    return "48 89 5C 24 10 48 89 74 24 18 48 89 7C 24 20 55 41 54 41 55 41 56 41 57 48 8D AC 24 40 FC FF FF 48 81 EC C0 04 00 00 48 8B ?? ?? ?? ?? ?? 48 33 C4 48 89 85 B0 03 00 00 4C 8B 29 48 8B C1"
+    return "48 89 5C 24 10 48 89 74 24 18 48 89 7C 24 20 55 41 54 41 55 41 56 41 57 48 8D AC 24 F0 FD FF FF 48 81 EC 10 03 00 00 48 8B ?? ?? ?? ?? ?? 48 33 C4 48 89 85 00 02 00 00"
 end
 
 function OnMatchFound(MatchAddress)
