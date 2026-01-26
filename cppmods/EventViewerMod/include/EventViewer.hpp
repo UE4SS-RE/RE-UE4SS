27547ab9829c42c43f92af2e448b747528f170f8
wildcherry2 <tylercwray@gmail.com>
2026-01-26 10:52:14 -0500

    Use register_tab in mod constructor with ImGui::Begin/EndDisabled until UE4SS is initialized

--- cppmods/EventViewerMod/include/EventViewer.hpp
+++ cppmods/EventViewerMod/include/EventViewer.hpp
@@ -5,16 +5,18 @@
 // UE4SS calls start_mod() (see dllmain.cpp) to create this mod instance. On Unreal init we register
 // a new ImGui tab and route rendering to Client.
 
-#include <concurrentqueue.h>
+#include <atomic>
 #include <Mod/CppUserModBase.hpp>
-#include <UE4SSProgram.hpp>
 
 namespace RC::EventViewerMod
 {
-    class EventViewerMod : public RC::CppUserModBase
+    class EventViewerMod : public CppUserModBase
     {
-      public:
+    public:
         EventViewerMod();
         auto on_unreal_init() -> void override;
+
+    private:
+        std::atomic_flag m_unreal_loaded{};
     };
 } // namespace RC::EventViewerMod
--- cppmods/EventViewerMod/src/EventViewer.cpp
+++ cppmods/EventViewerMod/src/EventViewer.cpp
@@ -7,36 +7,41 @@
 
 #include <Unreal/Hooks/Hooks.hpp>
 #include <Client.hpp>
-#include <DynamicOutput/Output.hpp>
-
-#include "UEngine.hpp"
+#include <UE4SSProgram.hpp>
+#include <UEngine.hpp>
 
 namespace RC::EventViewerMod
 {
-
     EventViewerMod::EventViewerMod()
     {
         ModName = STR("EventViewerMod");
         ModAuthors = STR("wildcherry");
-        ModDescription = STR("Lets you view a live call stack of ProcessEvent and ProcessInternal, with additional call frequency tracking.");
+        ModDescription = STR(
+            "Lets you view a live call stack of ProcessEvent and ProcessInternal, with additional call frequency tracking.");
         ModVersion = STR("1.0.0");
+
+        register_tab(STR("EventViewer"), [](CppUserModBase* mod)
+        {
+            UE4SS_ENABLE_IMGUI();
+
+            // Avoid unnecessary expensive dynamic_cast
+            ImGui::BeginDisabled(!static_cast<EventViewerMod*>(mod)->m_unreal_loaded.test()); // NOLINT(*-pro-type-static-cast-downcast)
+            auto& style = ImGui::GetStyle();
+            const auto old_size = style.GrabMinSize;
+            style.GrabMinSize = 30.0f;
+            Client::GetInstance().render();
+            style.GrabMinSize = old_size;
+            ImGui::EndDisabled();
+        });
     }
 
     void EventViewerMod::on_unreal_init()
     {
         Unreal::Hook::RegisterEngineTickPreCallback(
-                [this](auto&, Unreal::UEngine* e, float, bool) {
-                    register_tab(STR("EventViewer"), [](CppUserModBase* mod) {
-                        UE4SS_ENABLE_IMGUI();
-                        auto& style = ImGui::GetStyle();
-                        const auto old_size = style.GrabMinSize;
-                        style.GrabMinSize = 30.0f;
-                        Client::GetInstance().render();
-                        style.GrabMinSize = old_size;
-                    });
-                    // e->GetNamePrivate().GetComparisonIndex();
-                    Output::send<LogLevel::Verbose>(STR("Installed EventViewerMod GUI!"));
-                },
-                {true, true, STR("EventViewerMod"), STR("InstallHook")});
+            [this](auto&, Unreal::UEngine*, float, bool)
+            {
+                m_unreal_loaded.test_and_set();
+            },
+            {true, true, STR("EventViewerMod"), STR("InstallHook")});
     }
 } // namespace RC::EventViewerMod
