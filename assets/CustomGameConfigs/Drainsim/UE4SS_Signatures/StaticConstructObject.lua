265115c05bfcc75f96d8a1f4beda105a7d2753b1
noah the goodra <peterpan0413@live.com>
2026-03-30 23:44:09 -0500

    Add working drainsim signature and config (#1223)
    * Add working drainsim signature and config
    
    * Update assets/CustomGameConfigs/Drainsim/UE4SS-settings.ini
    
    Co-authored-by: UE4SS <91646798+UE4SS@users.noreply.github.com>
    
    * Update assets/CustomGameConfigs/Drainsim/UE4SS-settings.ini
    
    turn off permence penaty options
    
    Co-authored-by: UE4SS <91646798+UE4SS@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: UE4SS <91646798+UE4SS@users.noreply.github.com>

---
+++ assets/CustomGameConfigs/Drainsim/UE4SS_Signatures/StaticConstructObject.lua
@@ -0,0 +1,7 @@
+function Register()
+    return "4C 8B DC 55 53 41 56 49 8D AB ? ? ? ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? 8B 41"
+end
+
+function OnMatchFound(matchAddress)
+    return matchAddress
+end
