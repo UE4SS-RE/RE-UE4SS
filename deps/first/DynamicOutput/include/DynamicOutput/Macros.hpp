18dcb93499b00700916dc92277c281c84dc5180f
UE4SS <wildbrim@hotmail.com>
2026-04-07 09:50:43 +0200

    feat: OutputDevice build support for Linux
    Note that this has minimal testing.
    Specifically, running UE4SS on Windows has been tested and still works, and using DynamicOutput on Linux has been tested and is working except FileDevice and NewFileDevice.
    Nothing else has been tested.
    The File dependency on Linux is not implemented.

--- deps/first/DynamicOutput/include/DynamicOutput/Macros.hpp
+++ deps/first/DynamicOutput/include/DynamicOutput/Macros.hpp
@@ -1,6 +1,8 @@
 #ifndef DYNAMIC_OUTPUT_MACROS_HPP
 #define DYNAMIC_OUTPUT_MACROS_HPP
 
+#include <Helpers/String.hpp>
+
 #define ENABLE_OUTPUT_DEVICE_DEBUG_MODE 0
 
 #define ASSERT_OUTPUT_DEVICE_IS_VALID(param_device)                                                                                                            \
@@ -15,5 +17,15 @@
         THROW_INTERNAL_FILE_ERROR("[Output::send] Attempted to send but there were no default devices. Please specify at least one default device or "         \
                                   "construct a Targets object and supply your own devices.")                                                                   \
     }
+
+#ifdef _WIN32
+#define RC_DEVICE_PRINT_FUNC(fmt_var, optional_arg, optional_prefix) \
+wprintf_s(STR("%s%hs%ls\033[0m"), optional_prefix, log_level_to_color(static_cast<Color::Color>(optional_arg)).c_str(), m_formatter(fmt_var).c_str());
+#elif __linux__
+#define RC_DEVICE_PRINT_FUNC(fmt_var, optional_arg, optional_prefix) \
+const auto as_utf8 = to_utf8_string(m_formatter(fmt_var)); \
+const auto color_as_utf8 = to_utf8_string(log_level_to_color(static_cast<Color::Color>(optional_arg))); \
+printf("%s%s%s\033[0m", optional_prefix, color_as_utf8.c_str(), as_utf8.c_str());
+#endif
 
 #endif // DYNAMIC_OUTPUT_MACROS_HPP
