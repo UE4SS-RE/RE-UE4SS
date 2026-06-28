b993d72564aeaab82226d82848034c0bb713d31b
lazorr410 <nrosetti94+gamemods@gmail.com>
2026-06-28 07:58:33 -0400

    ghostwire-tokyo remove old signatures and add new ones

---
+++ assets/CustomGameConfigs/Ghost Wire Tokyo/UE4SS_Signatures/GUObjectHashTables.lua
@@ -0,0 +1,7 @@
+function Register()
+    return "48 83 EC 28 65 48 8B 04 25 58 00 00 00 BA F8 00 00 00 48 8B 08 8B 04 0A 39 05 BE 13 45 05 7F 0C 48 8D 05 BD 13 45 05 48 83 C4 28 C3"
+end
+
+function OnMatchFound(MatchAddress)
+    return MatchAddress
+end
