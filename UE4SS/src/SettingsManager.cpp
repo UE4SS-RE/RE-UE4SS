d3095e4f8700a08b665b1a718dd5cbb9812f18e8
lonercoder-x64 <lonercoder@gmail.com>
2026-07-02 11:24:24 +0400

    Update SettingsManager.cpp

--- UE4SS/src/SettingsManager.cpp
+++ UE4SS/src/SettingsManager.cpp
@@ -137,6 +137,8 @@ namespace RC
         REGISTER_BOOL_SETTING(Debug.DebugConsoleEnabled, section_debug, GuiConsoleEnabled)
         REGISTER_BOOL_SETTING(Debug.DebugConsoleVisible, section_debug, GuiConsoleVisible)
         REGISTER_FLOAT_SETTING(Debug.DebugGUIFontScaling, section_debug, GuiConsoleFontScaling)
+        REGISTER_STRING_SETTING(Debug.GuiCustomFontFile, section_debug, GuiCustomFontFile)
+        REGISTER_INT64_SETTING(Debug.GuiCustomFontSize, section_debug, GuiCustomFontSize)
         StringType graphics_api_string{};
         REGISTER_STRING_SETTING(graphics_api_string, section_debug, GraphicsAPI)
         if (String::iequal(graphics_api_string, STR("DX11")) || String::iequal(graphics_api_string, STR("D3D11")))
