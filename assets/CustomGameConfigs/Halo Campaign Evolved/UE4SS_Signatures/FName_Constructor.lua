f2352b4c44598f6409bebe0c51ff1a4adb07263c
lazorr410 <nrosetti94+gamemods@gmail.com>
2026-07-24 09:52:36 -0400

    chore: Halo Campaign Evolved signatures

---
+++ assets/CustomGameConfigs/Halo Campaign Evolved/UE4SS_Signatures/FName_Constructor.lua
@@ -0,0 +1,7 @@
+function Register()
+    return "48 89 5C 24 08 57 48 83 EC 30 41 8B F8 48 89 54 24 20 45 33 C0 48 8B D9 4C 8B CA 41 8B C8 48 85 D2 74 1E 0F B7 02"
+end
+
+function OnMatchFound(MatchAddress)
+    return MatchAddress
+end
