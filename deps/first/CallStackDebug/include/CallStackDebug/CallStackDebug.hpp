ee5cdd6e11d7f085167934881ef73609fb72c890
UE4SS <wildbrim@hotmail.com>
2026-08-31 16:32:09 +0200

    feat: Added get_unsymbolized_call_stack and symbolicate_call_stack
    get_unsymbolized_call_stack: The plan is to try to serialize these traces to file without symbols, alongside other information to allow devs to easily identify exactly with symbol file (PDB) is required for this particular stack trace.
    
    symbolicate_call_stack: The plan is to use this function to convert the module + RIP pairs into actual symbols so that devs can more easily debug problems.

--- deps/first/CallStackDebug/include/CallStackDebug/CallStackDebug.hpp
+++ deps/first/CallStackDebug/include/CallStackDebug/CallStackDebug.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <vector>
+
 #include <CallStackDebug/Common.hpp>
 #include <String/StringType.hpp>
 
@@ -13,6 +15,21 @@
 
 namespace RC::CallStackDebug
 {
-    auto RC_CALL_STACK_DEBUG_API generate_new_call_stack() -> void;
+    using ModuleNameAndOffsetPair = std::pair<StringType, size_t>;
+    using CallStackWithoutSymbols = std::vector<ModuleNameAndOffsetPair>;
+
+    // Generates a new call stack.
+    // Leave the CallStackWithoutSymbols param empty to use the current stack context.
+    auto RC_CALL_STACK_DEBUG_API generate_new_call_stack(const CallStackWithoutSymbols& = {}) -> void;
+
+    // Get the latest call stack trace.
+    // Remember to generate a trace before calling this function.
     auto RC_CALL_STACK_DEBUG_API get_call_stack() -> StringType;
+
+    // Returns a call stack without symbols attached, useful when debugging symbols are unavailable.
+    // A use-case would be to serialize module + RIP pairs for symbolication later when debugging symbols are available.
+    auto RC_CALL_STACK_DEBUG_API get_unsymbolized_call_stack() -> CallStackWithoutSymbols;
+
+    // Turns module + RIP pairs into a symbolized call stack.
+    auto RC_CALL_STACK_DEBUG_API symbolicate_call_stack(const CallStackWithoutSymbols&) -> StringType;
 } // namespace RC::CallStackDebug
--- deps/first/CallStackDebug/src/CallStackDebug.cpp
+++ deps/first/CallStackDebug/src/CallStackDebug.cpp
@@ -6,12 +6,15 @@
 #if RC_STACKTRACE_ENABLED
 #include <stacktrace>
 #endif
+#include <ranges>
+#include <functional>
 
 #ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
 #include <Windows.h>
 #include <DbgHelp.h>
+#include <Psapi.h>
 #else
 #error "CallStackDebug is only supported on Windows"
 #endif
@@ -30,31 +33,79 @@ namespace RC::CallStackDebug
         }
     };
 
-    auto generate_new_call_stack() -> void
+    static constexpr auto s_module_unknown = std::pair<uintptr_t, StringType>{std::numeric_limits<uintptr_t>::max(), {}};
+    static constexpr auto s_module_unknown_by_name = std::numeric_limits<uintptr_t>::max();
+
+    // Returns module base address and name that the given address belongs to.
+    static auto get_module_from_address(void* in_address) -> std::pair<uintptr_t, StringType>
     {
-        SymSetOptions(SYMOPT_UNDNAME | SYMOPT_DEFERRED_LOADS | SYMOPT_LOAD_LINES);
-        const HANDLE current_process = GetCurrentProcess();
-        HANDLE debug_handle{};
-        if (!DuplicateHandle(current_process, current_process, current_process, &debug_handle, 0, false, DUPLICATE_SAME_ACCESS))
+        const auto address = reinterpret_cast<uintptr_t>(in_address);
+        HMODULE modules[1024]{};
+        DWORD bytes_required{};
+        if (K32EnumProcessModules(GetCurrentProcess(), modules, sizeof(modules), &bytes_required) == 0)
         {
-            s_call_stack_cache = fmt::format(STR("Error when generating call stack: DuplicateHandle returned error: 0x{:X}\n"), GetLastError());
-            return;
+            return s_module_unknown;
         }
-        if (!SymInitialize(debug_handle, nullptr, true))
+
+        for (size_t i = 0; i < bytes_required / sizeof(HMODULE); ++i)
         {
-            s_call_stack_cache = fmt::format(STR("Error when generating call stack: SymInitialize returned error: 0x{:X}\n"), GetLastError());
-            return;
+            MODULEINFO info{};
+            K32GetModuleInformation(GetCurrentProcess(), modules[i], &info, sizeof(MODULEINFO));
+            const auto base = reinterpret_cast<uintptr_t>(info.lpBaseOfDll);
+            if (address >= base && address <= base + info.SizeOfImage)
+            {
+                char module_raw_name[MAX_PATH];
+                if (K32GetModuleBaseNameA(GetCurrentProcess(), modules[i], module_raw_name, sizeof(module_raw_name) / sizeof(char)) == 0)
+                {
+                    continue;
+                }
+                const auto module_name = ensure_str(module_raw_name);
+                return {base, module_name};
+            }
         }
-        auto cleanup = Cleanup([&]() {
-            SymCleanup(debug_handle);
-        });
-        s_call_stack_cache.clear();
-        const auto trace = std::stacktrace::current();
+
+        return s_module_unknown;
+    }
+
+    // Returns the base address of a module by name.
+    static auto get_module_base_from_name(StringViewType name) -> uintptr_t
+    {
+        HMODULE modules[1024]{};
+        DWORD bytes_required{};
+        if (K32EnumProcessModules(GetCurrentProcess(), modules, sizeof(modules), &bytes_required) == 0)
+        {
+            return s_module_unknown_by_name;
+        }
+
+        for (size_t i = 0; i < bytes_required / sizeof(HMODULE); ++i)
+        {
+            char module_raw_name[MAX_PATH];
+            if (K32GetModuleBaseNameA(GetCurrentProcess(), modules[i], module_raw_name, sizeof(module_raw_name) / sizeof(char)) == 0)
+            {
+                continue;
+            }
+            const auto module_name = ensure_str(module_raw_name);
+            if (name != module_name)
+            {
+                continue;
+            }
+
+            MODULEINFO info{};
+            K32GetModuleInformation(GetCurrentProcess(), modules[i], &info, sizeof(MODULEINFO));
+            const auto base = reinterpret_cast<uintptr_t>(info.lpBaseOfDll);
+            return base;
+        }
+
+        return s_module_unknown_by_name;
+    }
+
+    static auto iterate_stack_trace(HANDLE debug_handle, auto&& trace, auto frame_resolver) -> void
+    {
         // Skipping entry 0 because that's this function, and we're assuming the user is not interested in this code.
         for (size_t i = 1; i < trace.size(); ++i)
         {
             const auto& current_frame = trace[i];
-            const auto original_address = reinterpret_cast<uintptr_t>(current_frame.native_handle());
+            const uintptr_t original_address = frame_resolver(current_frame);
             {
                 s_call_stack_cache.append(fmt::format(STR("[{}] "), i - 1));
                 char buffer[sizeof(SYMBOL_INFO) + MAX_SYM_NAME * sizeof(TCHAR)];
@@ -83,12 +134,66 @@ namespace RC::CallStackDebug
         }
     }
 
+    auto generate_new_call_stack(const CallStackWithoutSymbols& call_stack) -> void
+    {
+        SymSetOptions(SYMOPT_UNDNAME | SYMOPT_DEFERRED_LOADS | SYMOPT_LOAD_LINES);
+        const HANDLE current_process = GetCurrentProcess();
+        HANDLE debug_handle{};
+        if (!DuplicateHandle(current_process, current_process, current_process, &debug_handle, 0, false, DUPLICATE_SAME_ACCESS))
+        {
+            s_call_stack_cache = fmt::format(STR("Error when generating call stack: DuplicateHandle returned error: 0x{:X}\n"), GetLastError());
+            return;
+        }
+        if (!SymInitialize(debug_handle, nullptr, true))
+        {
+            s_call_stack_cache = fmt::format(STR("Error when generating call stack: SymInitialize returned error: 0x{:X}\n"), GetLastError());
+            return;
+        }
+        auto cleanup = Cleanup([&]() {
+            SymCleanup(debug_handle);
+        });
+        s_call_stack_cache.clear();
+        if (!call_stack.empty())
+        {
+            iterate_stack_trace(debug_handle, call_stack, [](const ModuleNameAndOffsetPair& pair) {
+                return get_module_base_from_name(pair.first) + pair.second;
+            });
+        }
+        else
+        {
+            const auto trace = std::stacktrace::current();
+            iterate_stack_trace(debug_handle, trace, [](const std::stacktrace_entry& entry) {
+                return reinterpret_cast<uintptr_t>(entry.native_handle());
+            });
+        }
+    }
+
     auto get_call_stack() -> StringType
     {
         if (s_call_stack_cache.empty())
         {
             generate_new_call_stack();
         }
+        return s_call_stack_cache;
+    }
+
+
+    auto get_unsymbolized_call_stack() -> CallStackWithoutSymbols
+    {
+        CallStackWithoutSymbols call_stack{};
+        const auto trace = std::stacktrace::current();
+        for (const auto& entry : trace)
+        {
+            const auto [module_base, module_name] = get_module_from_address(entry.native_handle());
+            const auto module_offset = reinterpret_cast<uintptr_t>(entry.native_handle()) - module_base;
+            call_stack.emplace_back(module_name, module_offset);
+        }
+        return call_stack;
+    }
+
+    auto symbolicate_call_stack(const CallStackWithoutSymbols& call_stack) -> StringType
+    {
+        generate_new_call_stack(call_stack);
         return s_call_stack_cache;
     }
 } // namespace RC::PDB
